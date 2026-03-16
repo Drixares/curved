9f6b9daf284d6bee1b3f45e63770ff65556a4675
Mattéo <matteo.lorenzoq@gmail.com>
2026-03-16 11:27:24 +0100

    chore: setup postgres, drizzle and better-auth

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { auth } from './lib/auth'
 
 const app = new Hono()
 
@@ -12,6 +13,10 @@ app.use(
 )
 
 app.get('/', (c) => c.json({ message: 'Curved API' }))
+
+app.on(['POST', 'GET'], '/api/auth/**', (c) => {
+  return auth.handler(c.req.raw)
+})
 
 export default {
   port: 3000,
