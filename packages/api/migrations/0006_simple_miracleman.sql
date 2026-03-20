57fa2bb893f5d440a602956b1f6c274eff7cffbb
Mattéo <matteo.lorenzoq@gmail.com>
2026-03-20 10:47:07 +0100

    fix: migration (v3)

--- packages/api/migrations/0006_simple_miracleman.sql
+++ packages/api/migrations/0006_simple_miracleman.sql
@@ -1,4 +1,8 @@
 DROP TABLE IF EXISTS "issue_project" CASCADE;--> statement-breakpoint
-ALTER TABLE "issue" ADD COLUMN "project_id" text;--> statement-breakpoint
-ALTER TABLE "issue" ADD CONSTRAINT "issue_project_id_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."project"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
-CREATE INDEX "issue_projectId_idx" ON "issue" USING btree ("project_id");
+ALTER TABLE "issue" ADD COLUMN IF NOT EXISTS "project_id" text;--> statement-breakpoint
+DO $$ BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'issue_project_id_project_id_fk') THEN
+    ALTER TABLE "issue" ADD CONSTRAINT "issue_project_id_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."project"("id") ON DELETE set null ON UPDATE no action;
+  END IF;
+END $$;--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "issue_projectId_idx" ON "issue" USING btree ("project_id");
