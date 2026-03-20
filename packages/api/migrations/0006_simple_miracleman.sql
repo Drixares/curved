6e6d11652699277f07c19568f31379f3312a4951
Mattéo <matteo.lorenzoq@gmail.com>
2026-03-20 10:41:10 +0100

    fix: migrations

--- packages/api/migrations/0006_simple_miracleman.sql
+++ packages/api/migrations/0006_simple_miracleman.sql
@@ -1,5 +1,4 @@
-ALTER TABLE "issue_project" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "issue_project" CASCADE;--> statement-breakpoint
+DROP TABLE IF EXISTS "issue_project" CASCADE;--> statement-breakpoint
 ALTER TABLE "issue" ADD COLUMN "project_id" text;--> statement-breakpoint
 ALTER TABLE "issue" ADD CONSTRAINT "issue_project_id_project_id_fk" FOREIGN KEY ("project_id") REFERENCES "public"."project"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "issue_projectId_idx" ON "issue" USING btree ("project_id");
